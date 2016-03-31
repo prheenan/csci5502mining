8d193b8f0b63698bfc4e8f8244271ab8dc0dc395
pfsalminen <pasa0219@colorado.edu>
2016-03-31 00:13:04 -0600

    Created FeatureUtils. Cleans up FeatureFunctions.

---
+++ DataMining/_3_ConvertToFeatures/FeatureUtils.py
@@ -0,0 +1,72 @@
+# Force floating point division. Can still use integer with //
+from __future__ import division
+# This file is used for importing the common utilities classes
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+def TestFunction(obj):
+	'''
+	Returns a flattened window
+	Args:
+		obj: preprocessed object with the windows we want
+	Returns a flattaned list of hi res data from the window
+	'''
+	return [data for window in obj.HiResData.HiResData\
+			for data in window]
+
+def grad_std(filtered_obj):
+	'''
+	Normalizes to a standard curve
+	'''
+	filteredGradient = np.gradient(filtered_ob)
+	stdV = np.std(filteredGradient)
+	zGrad = (filteredGradient - np.mean(filteredGradient))/stdV
+	return zGrad
+
+def grad_minmax(filtered_obj):
+	'''
+	Normalizes via Min-Max
+	'''
+	filteredGradient = np.gradient(filtered_obj)
+	norm = (filteredGradient - filteredGradient.min()) / (filteredGradient.max() - filteredGradient.min())
+	return norm
+
+def dfFeatureGen(obj, data_type, norm):
+	'''
+	Function finds features of force derivative objects based on arguments
+	Args:
+		obj : Preprocessed data
+		data_type : Type of data give. Options - 'force' or 'separation'
+		norm : Type of normalization fo be used. Options - 'std' or 'minmax'
+	'''
+	progs = {
+			'std' : grad_std,
+			'minmax' : grad_minmax
+			}
+	features = list()
+	time,sepWindow,forceWindow = \
+		obj.HighResData.GetTimeSepForce()
+	nWindows = len(sepWindow)
+	timeConst = 8e-5	#Keep this in?
+	mFiltering = FilterObj.Filter(timeConst = timeConst)
+	if data_type.lower() == 'force':
+		for i,(time,sep,force) in enumerate(zip(timeWindow,sepWindow,forceWindow)):
+			# Should we keep the first lines in if its just for plotting?
+			force *= 1e12
+			deltaT = time[1] - time[0]
+			startIdxInWindow = int((labels[i].StartTime - time[0]) / (deltaT))
+			force -=np.median(force[:startIdxInWindow])
+			filteredForce = mFiltering.FilterDataY(time,force)
+			features.append(progs[norm](filteredForce))
+
+	elif data_type.lower() == 'separation':
+		for i,(time,sep,force) in enumerate(zip(timeWindow,sepWindow,forceWindow)):
+			filteredSep = mFiltering.FilterDataY(time,sep)
+			features.append(progs[norm](filteredSep))
+
+	else:
+		print "Error: Invalid data type input. Valid inputs are 'force' or 'separation'"
+		return
+
+	return features
